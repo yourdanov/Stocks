c7fce2779ed8e49e85c7fb07d994d7c187b89cad
Atanas Yourdanov <113124786+yourdanov@users.noreply.github.com>
2025-01-15 10:55:37 +0200

    import section updated in stocks_forecast.py...
    import section updated in stocks_forecast.py...

--- stocks_forecast.py
+++ stocks_forecast.py
@@ -3,29 +3,29 @@ os.environ["TF_CPP_MIN_LOG_LEVEL"] = "2"  # Suppress TensorFlow INFO/WARNING log
 os.environ["TF_ENABLE_ONEDNN_OPTS"] = "0"  # Disable oneDNN optimizations
 
 import yfinance as yf
-from datetime import datetime
 import pandas as pd
+import tkinter as tk
+import matplotlib.pyplot as plt
+import numpy as np
+import tensorflow as tf
+import tensorflow.keras.backend as K # type: ignore
+import matplotlib.dates
+import mplcursors
+import nltk
+from datetime import datetime
 from statsmodels.tsa.arima.model import ARIMA
 from statsmodels.tsa.holtwinters import ExponentialSmoothing
 from sklearn.ensemble import RandomForestRegressor
 from sklearn.model_selection import train_test_split
-import tkinter as tk
 from tkinter import Label, Button
 from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg, NavigationToolbar2Tk
-import matplotlib.pyplot as plt
-import numpy as np
-import tensorflow as tf
 from tensorflow.keras.models import Sequential # type: ignore
 from tensorflow.keras.layers import LSTM, Dense, Dropout # type: ignore
 from tensorflow.keras.callbacks import EarlyStopping # type: ignore
-import tensorflow.keras.backend as K # type: ignore
 from sklearn.preprocessing import MinMaxScaler
 from statsmodels.tsa.stattools import adfuller
 from pmdarima import auto_arima
 from datetime import datetime
-import matplotlib.dates
-import mplcursors
-import nltk
 
 nltk.download("vader_lexicon", quiet=True)  # Suppress download message
 
